04288f9bea4d1c631d15b4ebe868731fc7ac30d1
tou0jw9T <67032045+tou0jw9T@users.noreply.github.com>
2020-06-17 16:05:29 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
-basic.showIcon(IconNames.Heart)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == a || receivedNumber != a) {
+        basic.showIcon(IconNames.Asleep)
+    } else {
+        if (true) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Angry)
+        }
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    a = 1
+    radio.sendNumber(a)
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+    a = 3
+    radio.sendNumber(9)
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    a = 2
+    radio.sendNumber(a)
+    basic.showIcon(IconNames.Square)
+})
+let a = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-	
+    radio.setGroup(9)
 })
