61b710fd68853b19ca266157ec26589d8a74adb3
tou0jw9T <67032045+tou0jw9T@users.noreply.github.com>
2020-06-24 16:00:41 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,29 +7,46 @@ radio.onReceivedNumber(function (receivedNumber) {
         index += 1000
     }
     if (index == 1110 || (index == 30 || (index == 300 || (index == 3000 || (index == 20 || (index == 200 || index == 2000)))))) {
-        basic.showIcon(IconNames.Asleep)
+        for (let index2 = 0; index2 < 4; index2++) {
+            basic.showIcon(IconNames.Asleep)
+        }
     } else if (a == 1) {
         if (index == 210 || (index == 120 || index == 1010)) {
-            basic.showIcon(IconNames.Happy)
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Happy)
+            }
         } else {
-            basic.showIcon(IconNames.Angry)
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Angry)
+            }
         }
     } else if (a == 2) {
         if (index == 1020 || (index == 2010 || index == 110)) {
-            basic.showIcon(IconNames.Happy)
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Happy)
+            }
         } else {
-            basic.showIcon(IconNames.Angry)
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Angry)
+            }
         }
     } else if (a == 3) {
         if (index == 1200 || (index == 2100 || index == 1100)) {
-            basic.showIcon(IconNames.Happy)
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Happy)
+            }
         } else {
-            basic.showIcon(IconNames.Angry)
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Angry)
+            }
         }
     }
+    a = 0
+    index = 0
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
     a = 1
     index = 10
     radio.sendNumber(1)
@@ -37,15 +54,18 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Yes)
     a = 0
+    index = 0
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Scissors)
+    basic.clearScreen()
     a = 3
     index = 1000
     radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Chessboard)
+    basic.clearScreen()
     a = 2
     index = 100
     radio.sendNumber(2)
