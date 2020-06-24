57d19647e6dfb10b87bd775ae7e82e9022c6230e
tou0jw9T <67032045+tou0jw9T@users.noreply.github.com>
2020-06-24 12:04:20 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == a || receivedNumber != a) {
+    if (receivedNumber == 1) {
+        index += 10
+    } else if (receivedNumber == 2) {
+        index += 100
+    } else if (receivedNumber == 3) {
+        index += 1000
+    }
+    if (index == 1110 || (index == 30 || (index == 300 || (index == 3000 || (index == 20 || (index == 200 || index == 2000)))))) {
         basic.showIcon(IconNames.Asleep)
-    } else {
-        if (true) {
+    } else if (a == 1) {
+        if (index == 210 || (index == 120 || index == 1010)) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Angry)
+        }
+    } else if (a == 2) {
+        if (index == 1020 || (index == 2010 || index == 110)) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Angry)
+        }
+    } else if (a == 3) {
+        if (index == 1200 || (index == 2100 || index == 1100)) {
             basic.showIcon(IconNames.Happy)
         } else {
             basic.showIcon(IconNames.Angry)
@@ -10,24 +29,29 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onButtonPressed(Button.A, function () {
-    a = 1
-    radio.sendNumber(a)
     basic.showIcon(IconNames.SmallSquare)
+    a = 1
+    index = 10
+    radio.sendNumber(1)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Yes)
+    a = 0
 })
 input.onButtonPressed(Button.AB, function () {
-    a = 3
-    radio.sendNumber(9)
     basic.showIcon(IconNames.Scissors)
+    a = 3
+    index = 1000
+    radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Chessboard)
     a = 2
-    radio.sendNumber(a)
-    basic.showIcon(IconNames.Square)
+    index = 100
+    radio.sendNumber(2)
 })
 let a = 0
+let index = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     radio.setGroup(9)
