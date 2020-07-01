960ed302124f940e71496943851d6ef600e3a381
tou0jw9T <67032045+tou0jw9T@users.noreply.github.com>
2020-07-01 10:13:11 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,77 +1,50 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        index += 10
-    } else if (receivedNumber == 2) {
-        index += 100
-    } else if (receivedNumber == 3) {
-        index += 1000
-    }
-    if (index == 1110 || (index == 30 || (index == 300 || (index == 3000 || (index == 20 || (index == 200 || index == 2000)))))) {
-        for (let index2 = 0; index2 < 4; index2++) {
-            basic.showIcon(IconNames.Asleep)
-        }
-    } else if (a == 1) {
-        if (index == 210 || (index == 120 || index == 1010)) {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.showIcon(IconNames.Happy)
-            }
-        } else {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.showIcon(IconNames.Angry)
-            }
-        }
+    if (receivedNumber * a == 4 || (receivedNumber * a == 9 || (receivedNumber * a == 25 || (receivedNumber * a == 8 || (receivedNumber * a == 27 || (receivedNumber * a == 125 || receivedNumber * a == 30)))))) {
+        basic.showIcon(IconNames.Asleep)
     } else if (a == 2) {
-        if (index == 1020 || (index == 2010 || index == 110)) {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.showIcon(IconNames.Happy)
-            }
+        if (receivedNumber * a == 6 || (receivedNumber * a == 12 || receivedNumber * a == 18)) {
+            basic.showIcon(IconNames.Happy)
         } else {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.showIcon(IconNames.Angry)
-            }
+            basic.showIcon(IconNames.Angry)
         }
     } else if (a == 3) {
-        if (index == 1200 || (index == 2100 || index == 1100)) {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.showIcon(IconNames.Happy)
-            }
+        if (receivedNumber * a == 15 || (receivedNumber * a == 45 || receivedNumber * a == 75)) {
+            basic.showIcon(IconNames.Happy)
         } else {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.showIcon(IconNames.Angry)
-            }
+            basic.showIcon(IconNames.Angry)
+        }
+    } else if (a == 5) {
+        if (receivedNumber * a == 10 || (receivedNumber * a == 20 || receivedNumber * a == 50)) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Angry)
         }
     }
     a = 0
-    index = 0
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
-    a = 1
-    index = 10
-    radio.sendNumber(1)
+    a = 2
+    radio.sendNumber(2)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Yes)
     a = 0
-    index = 0
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Scissors)
     basic.clearScreen()
     a = 3
-    index = 1000
     radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Chessboard)
     basic.clearScreen()
-    a = 2
-    index = 100
-    radio.sendNumber(2)
+    a = 5
+    radio.sendNumber(5)
 })
 let a = 0
-let index = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     radio.setGroup(9)
